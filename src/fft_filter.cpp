8dc46d4991fa11a276ee73ce41e05439ee380183
Julius Kammerl <julius@kammerl.de>
2014-04-19 22:47:26 -0700

    code style formatting

--- src/fft_filter.cpp
+++ src/fft_filter.cpp
@@ -5,111 +5,115 @@
 
 using namespace std;
 
-FFTFilter::FFTFilter(int filter_len) :
-		filter_len_(filter_len),
-		fft_len_(filter_len * 2),
-		filter_state_(filter_len, 0.0f),
-		kernel_defined_(false),
-		kernel_time_domain_buffer_(fft_len_),
-		kernel_freq_domain_buffer_(fft_len_ / 2 + 1),
-		buffer_selector_(0),
-		signal_time_domain_buffer_(2, vector<kiss_fft_scalar>(fft_len_)),
-		signal_freq_domain_buffer_(2, vector<kiss_fft_cpx>(fft_len_ / 2 + 1)),
-		filtered_freq_domain_buffer_(fft_len_ / 2 + 1) {
-	bool is_power_of_two = ((fft_len_ != 0) && !(fft_len_ & (fft_len_ - 1)));
-	assert(is_power_of_two && "Filter length must be a power of 2");
+FFTFilter::FFTFilter(int filter_len)
+    : filter_len_(filter_len),
+      fft_len_(filter_len * 2),
+      filter_state_(filter_len, 0.0f),
+      kernel_defined_(false),
+      kernel_time_domain_buffer_(fft_len_),
+      kernel_freq_domain_buffer_(fft_len_ / 2 + 1),
+      buffer_selector_(0),
+      signal_time_domain_buffer_(2, vector<kiss_fft_scalar>(fft_len_)),
+      signal_freq_domain_buffer_(2, vector<kiss_fft_cpx>(fft_len_ / 2 + 1)),
+      filtered_freq_domain_buffer_(fft_len_ / 2 + 1) {
+  bool is_power_of_two = ((fft_len_ != 0) && !(fft_len_ & (fft_len_ - 1)));
+  assert(is_power_of_two && "Filter length must be a power of 2");
 
-	forward_fft_ = kiss_fftr_alloc(fft_len_, 0, 0, 0);
-	inverse_fft_ = kiss_fftr_alloc(fft_len_, 1, 0, 0);
+  forward_fft_ = kiss_fftr_alloc(fft_len_, 0, 0, 0);
+  inverse_fft_ = kiss_fftr_alloc(fft_len_, 1, 0, 0);
 
-	Init();
+  Init();
 }
 
 FFTFilter::~FFTFilter() {
-	kiss_fft_free(forward_fft_);
-	kiss_fft_free(inverse_fft_);
+  kiss_fft_free(forward_fft_);
+  kiss_fft_free(inverse_fft_);
 }
 
 void FFTFilter::Init() {
-	// Initialize all buffers with zeros.
-	memset(&kernel_time_domain_buffer_[0], 0, sizeof(kiss_fft_scalar) * fft_len_);
-	memset(&kernel_freq_domain_buffer_[0], 0, sizeof(kiss_fft_cpx) * (fft_len_ / 2 + 1));
-	for (int i = 0; i < 2; ++i) {
-		memset(&signal_time_domain_buffer_[i][0], 0,
-				sizeof(kiss_fft_scalar) * fft_len_);
-		memset(&signal_freq_domain_buffer_[i][0], 0,
-				sizeof(kiss_fft_cpx) * (fft_len_ / 2 + 1));
-	}
+  // Initialize all buffers with zeros.
+  memset(&kernel_time_domain_buffer_[0], 0, sizeof(kiss_fft_scalar) * fft_len_);
+  memset(&kernel_freq_domain_buffer_[0], 0,
+         sizeof(kiss_fft_cpx) * (fft_len_ / 2 + 1));
+  for (int i = 0; i < 2; ++i) {
+    memset(&signal_time_domain_buffer_[i][0], 0,
+           sizeof(kiss_fft_scalar) * fft_len_);
+    memset(&signal_freq_domain_buffer_[i][0], 0,
+           sizeof(kiss_fft_cpx) * (fft_len_ / 2 + 1));
+  }
 }
 
 void FFTFilter::SetKernel(const std::vector<float>& kernel) {
-	assert(kernel.size()==filter_len_ && "Kernel size must match filter length");
+  assert(kernel.size()==filter_len_ && "Kernel size must match filter length");
 
-	for (int i = 0; i < filter_len_; ++i) {
-		kernel_time_domain_buffer_[i] = kernel[i];
-	}
-	// Zero padding
-	for (int i = filter_len_; i < fft_len_; ++i) {
-		kernel_time_domain_buffer_[i] = 0.0f;
-	}
+  for (int i = 0; i < filter_len_; ++i) {
+    kernel_time_domain_buffer_[i] = kernel[i];
+  }
+  // Zero padding
+  for (int i = filter_len_; i < fft_len_; ++i) {
+    kernel_time_domain_buffer_[i] = 0.0f;
+  }
 
-	// Perform forward FFT transform
-	kiss_fftr(forward_fft_, &kernel_time_domain_buffer_[0],
-			&kernel_freq_domain_buffer_[0]);
+  // Perform forward FFT transform
+  kiss_fftr(forward_fft_, &kernel_time_domain_buffer_[0],
+            &kernel_freq_domain_buffer_[0]);
 
-	kernel_defined_ = true;
+  kernel_defined_ = true;
 }
 
 void FFTFilter::AddSignalBlock(const vector<float>& signal_block) {
-	assert(signal_block.size()==filter_len_ && "Signal block size must match filter length");
-	assert(kernel_defined_ && "No suitable kernel defined");
+  assert(
+      signal_block.size()==filter_len_ && "Signal block size must match filter length");
+  assert(kernel_defined_ && "No suitable kernel defined");
 
-	// Switch buffer selector
-	buffer_selector_ = !buffer_selector_;
+  // Switch buffer selector
+  buffer_selector_ = !buffer_selector_;
 
-	vector<kiss_fft_scalar>& time_domain_buffer = signal_time_domain_buffer_[buffer_selector_];
-	vector<kiss_fft_cpx>& freq_domain_buffer = signal_freq_domain_buffer_[buffer_selector_];
+  vector<kiss_fft_scalar>& time_domain_buffer =
+      signal_time_domain_buffer_[buffer_selector_];
+  vector<kiss_fft_cpx>& freq_domain_buffer =
+      signal_freq_domain_buffer_[buffer_selector_];
 
+  for (int i = 0; i < filter_len_; ++i) {
+    time_domain_buffer[i] = signal_block[i];
+  }
+  // Zero padding
+  for (int i = filter_len_; i < fft_len_; ++i) {
+    time_domain_buffer[i] = 0.0f;
+  }
 
-	for (int i = 0; i < filter_len_; ++i) {
-		time_domain_buffer[i] = signal_block[i];
-	}
-	// Zero padding
-	for (int i = filter_len_; i < fft_len_; ++i) {
-		time_domain_buffer[i] = 0.0f;
-	}
+  // Perform forward FFT transform
+  kiss_fftr(forward_fft_, &time_domain_buffer[0], &freq_domain_buffer[0]);
 
-	// Perform forward FFT transform
-	kiss_fftr(forward_fft_, &time_domain_buffer[0], &freq_domain_buffer[0]);
+  // Complex multiplication in frequency domain with transformed kernel.
+  for (int i = 0; i < filtered_freq_domain_buffer_.size(); ++i) {
+    filtered_freq_domain_buffer_[i].r = freq_domain_buffer[i].r
+        * kernel_freq_domain_buffer_[i].r
+        - freq_domain_buffer[i].i * kernel_freq_domain_buffer_[i].i;
+    filtered_freq_domain_buffer_[i].i = freq_domain_buffer[i].r
+        * kernel_freq_domain_buffer_[i].i
+        + freq_domain_buffer[i].i * kernel_freq_domain_buffer_[i].r;
+  }
 
-	// Complex multiplication in frequency domain with transformed kernel.
-	for (int i = 0; i < filtered_freq_domain_buffer_.size(); ++i) {
-		filtered_freq_domain_buffer_[i].r =
-				freq_domain_buffer[i].r * kernel_freq_domain_buffer_[i].r -
-				freq_domain_buffer[i].i * kernel_freq_domain_buffer_[i].i;
-		filtered_freq_domain_buffer_[i].i =
-				freq_domain_buffer[i].r * kernel_freq_domain_buffer_[i].i +
-				freq_domain_buffer[i].i * kernel_freq_domain_buffer_[i].r;
-	}
+  // Perform inverse FFT transform of filtered_freq_domain_buffer_ and store result back in signal_time_domain_buffer_
+  kiss_fftri(inverse_fft_, &filtered_freq_domain_buffer_[0],
+             &time_domain_buffer[0]);
 
-	// Perform inverse FFT transform of filtered_freq_domain_buffer_ and store result back in signal_time_domain_buffer_
-	kiss_fftri(inverse_fft_, &filtered_freq_domain_buffer_[0], &time_domain_buffer[0]);
-
-	// Invert FFT scaling
-	for (int i = 0; i < fft_len_; ++i) {
-		time_domain_buffer[i] /= fft_len_;
-	}
+  // Invert FFT scaling
+  for (int i = 0; i < fft_len_; ++i) {
+    time_domain_buffer[i] /= fft_len_;
+  }
 }
 
 void FFTFilter::GetResult(vector<float>* signal_block) {
-	assert(signal_block);
-	signal_block->resize(filter_len_);
+  assert(signal_block);
+  signal_block->resize(filter_len_);
 
-	int curr_buf = buffer_selector_;
-	int prev_buf = !buffer_selector_;
-	for (int i = 0; i < filter_len_; ++i) {
-		(*signal_block)[i] = signal_time_domain_buffer_[curr_buf][i]
-				+ signal_time_domain_buffer_[prev_buf][i + filter_len_]; // Add overlap from previous FFT transform.
-	}
+  int curr_buf = buffer_selector_;
+  int prev_buf = !buffer_selector_;
+  for (int i = 0; i < filter_len_; ++i) {
+    (*signal_block)[i] = signal_time_domain_buffer_[curr_buf][i]
+        + signal_time_domain_buffer_[prev_buf][i + filter_len_];  // Add overlap from previous FFT transform.
+  }
 }
 
--- test/test_fft.cpp
+++ test/test_fft.cpp
@@ -7,35 +7,35 @@
 using namespace std;
 
 TEST(FFTTest, DiracImpulseTest) {
-	int fft_len = 64;
+  int fft_len = 64;
 
-	kiss_fftr_cfg forward_fft = kiss_fftr_alloc(fft_len, 0, 0, 0);
-	kiss_fftr_cfg inverse_fft = kiss_fftr_alloc(fft_len, 1, 0, 0);
+  kiss_fftr_cfg forward_fft = kiss_fftr_alloc(fft_len, 0, 0, 0);
+  kiss_fftr_cfg inverse_fft = kiss_fftr_alloc(fft_len, 1, 0, 0);
 
-	vector<float> test_signal(fft_len, 0.0f);
-	test_signal[fft_len / 2] = 1.0f;
+  vector<float> test_signal(fft_len, 0.0f);
+  test_signal[fft_len / 2] = 1.0f;
 
-	vector<kiss_fft_scalar> fft_time_domain_buffer(fft_len);
-	vector<kiss_fft_cpx> fft_freq_domain_buffer(fft_len / 2 + 1);
+  vector<kiss_fft_scalar> fft_time_domain_buffer(fft_len);
+  vector<kiss_fft_cpx> fft_freq_domain_buffer(fft_len / 2 + 1);
 
-	for (int i = 0; i < fft_len; ++i) {
-		fft_time_domain_buffer[i] = i;
-	}
+  for (int i = 0; i < fft_len; ++i) {
+    fft_time_domain_buffer[i] = i;
+  }
 
-	kiss_fftr(forward_fft, &fft_time_domain_buffer[0],
-			&fft_freq_domain_buffer[0]);
-	kiss_fftri(inverse_fft, &fft_freq_domain_buffer[0],
-			&fft_time_domain_buffer[0]);
+  kiss_fftr(forward_fft, &fft_time_domain_buffer[0],
+            &fft_freq_domain_buffer[0]);
+  kiss_fftri(inverse_fft, &fft_freq_domain_buffer[0],
+             &fft_time_domain_buffer[0]);
 
-	// Invert FFT scaling
-	for (int i = 0; i < fft_len; ++i) {
-		fft_time_domain_buffer[i] /= fft_len;
-	}
+  // Invert FFT scaling
+  for (int i = 0; i < fft_len; ++i) {
+    fft_time_domain_buffer[i] /= fft_len;
+  }
 
-	for (int i = 0; i < fft_len; ++i) {
-		EXPECT_NEAR(fft_time_domain_buffer[i], i, 1e-4);
-	}
+  for (int i = 0; i < fft_len; ++i) {
+    EXPECT_NEAR(fft_time_domain_buffer[i], i, 1e-4);
+  }
 
-	kiss_fft_free(forward_fft);
-	kiss_fft_free(inverse_fft);
+  kiss_fft_free(forward_fft);
+  kiss_fft_free(inverse_fft);
 }
